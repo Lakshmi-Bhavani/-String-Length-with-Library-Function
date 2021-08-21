99a195054d1b62c36531d14df34874fdf2dad5ce
Lakshmi-Bhavani <84325499+Lakshmi-Bhavani@users.noreply.github.com>
2021-08-21 21:35:34 +0530

    Add files via upload

---
+++ 8a3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char str1[]="Program";
+	char str2[]={'P','r','o','g','r','a','m','\0'};
+	printf("Length of string str1=%d",strlen(str1));
+	printf("\nLength of string str2=%d",strlen(str2));
+	
+}
